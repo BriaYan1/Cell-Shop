323e6fcb8aec0fd422550bde64dca077e227bb4d
Briayan <briayang1997@gmail.com>
2024-08-24 15:07:44 -0400

    Vista carrito funcionando

--- vista_principal/admin.py
+++ vista_principal/admin.py
@@ -3,8 +3,8 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(cliente)
-admin.site.register(producto)
-admin.site.register(orden)
-admin.site.register(orden_items)
-admin.site.register(datos_envio)
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Orden)
+admin.site.register(OrdenItems)
+admin.site.register(DatosEnvio)
